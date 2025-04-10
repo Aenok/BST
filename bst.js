60a3cf660b739ae888dc99d09e605819c4570996
Ryan Stutz <ryan.stutz06@gmail.com>
2025-04-10 09:10:58 -0400

    implemented delete function

--- bst.js
+++ bst.js
@@ -20,42 +20,128 @@ class Tree {
         let newNode = new Node(value);
         while(true) {
             if(searchNode._data > value) {
+                // go left
                 if(searchNode.left == null) {
+                    // value is less than lowest leaft in this path - make it new leaf and set left pointer of searchNode to newNode
                     searchNode.left = newNode;
                     break;
                 } else {
+                    // search further left
                     searchNode = searchNode.left;
                 }                
             } else if (searchNode._data < value) {
+                // value is more than lowest leaft in this path - make it new leaf and set right pointer of searchNode to newNode
                 if(searchNode.right == null) {
                     searchNode.right = newNode;
                     break;
                 } else {
+                    // search further right
                     searchNode = searchNode.right;
                 }
             } else {
+                // covering the edge case where were trying to insert a value already found in the BST
                 return; 
             }
         }
     }
 
+    delete(value) {
+        let searchResults = this.find(value);
+        // console.log(searchResults);
+        if(searchResults.searchNode == null) {
+            // value not in BST, return from function
+            return;
+        } else if(searchResults.searchNode.left == null && searchResults.searchNode.right == null) {
+            // value we are deleting is a leaf, children dont need to be accounted for
+            if(searchResults.searchNode._data > searchResults.prevNode._data) {
+                // searchResults data is greater than its parent -> searchResults is the right child of prevNode
+                searchResults.prevNode.right = null;
+            } else {
+                // searchResults data is less than its parent -> searchResults is the left child of prevNode
+                searchResults.prevNode.left = null;
+            }
+        } else if((searchResults.searchNode.left == null && searchResults.searchNode.right != null) || (searchResults.searchNode.left != null && searchResults.searchNode.right == null)) {
+            // value we are deleting has 1 child
+            if(searchResults.searchNode.left == null) {
+                // the child is on the right
+                if(searchResults.searchNode._data > searchResults.prevNode._data) {
+                    // searchResults node is the right child of prevNode -> make searchResults right child the right child of prevNode
+                    searchResults.prevNode.right = searchResults.searchNode.right;
+                } else {
+                    // searchResults node is the left child of prevNode -> make searchResults right child the left child of prevNode
+                    searchResults.prevNode.left = searchResults.searchNode.right;
+
+                }
+            } else {
+                // the child is on the left
+                if(searchResults.searchNode._data > searchResults.prevNode._data) {
+                    // searchResults node is the right child of prevNode -> make searchResults left child the right child of prevNode
+                    searchResults.prevNode.right = searchResults.searchNode.left;
+                } else {
+                    //searchResults node is the left child of prevNode -> make searchResults left child the left child of prevNode
+                    searchResults.prevNode.left = searchResults.searchNode.left;
+                }
+            }
+        } else {
+            // value we are deleting has 2 children
+            let parent = searchResults.prevNode;
+            let delNode = searchResults.searchNode;
+            let prev;
+            let repNode = delNode.right;
+            while(repNode.left != null) {
+                // console.log(`repNode data is = ${repNode._data}`);
+                prev = repNode;
+                repNode = repNode.left;
+            }
+
+            if (prev != null) {
+                //if prev != null, then we had to go down a series of nodes to reach the repNode. we need to sete repNodes parents left to null to avoid circular chains
+                prev.left = null;
+            }
+
+            if(delNode._data > parent._data) {
+                //delNode is right child of parent
+                parent.right = repNode;                
+            } else {
+                //delNode is left child of parent
+                parent.left = repNode;
+            }
+
+            // console.log(delNode)
+
+            repNode.left = delNode.left;
+            // console.log(repNode);
+            if(delNode.right == repNode) {
+                // repNode is the only shift from delNode and there are no further nodes down the chain, therefore repNode will have no right child
+                repNode.right = null;
+            } else {
+                repNode.right = delNode.right;
+            }
+            // console.log(repNode);
+        }
+        searchResults.searchNode = null;
+    }
+
 
     find(value) {
+        let prevNode;
         let searchNode = this.root;
 
         while(searchNode != null) {
             
             if(searchNode._data == value) {
-                return searchNode;
+                return { prevNode, searchNode };
             } else if(searchNode._data > value) {
                 // check left
+                prevNode = searchNode;
                 searchNode = searchNode.left;
             } else if(searchNode._data < value) {
                 //check right
+                prevNode = searchNode;
                 searchNode = searchNode.right;
             }
         }
-        return searchNode;
+        return { prevNode, searchNode };
     }
 }
 
@@ -117,8 +203,8 @@ let arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 let tree = new Tree(arr);
 tree.root = buildTree(arr);
 
-// let node = tree.find(12);
-// console.log(node);
+// let searchResults = tree.find(7);
+// console.log(searchResults);
 
 
 
@@ -146,7 +232,16 @@ tree.root = buildTree(arr);
 // prettyPrint(tree.root);
 // console.log("------------------")
 
-
+tree.insert(10000);
+tree.insert(8000);
+tree.insert(9000);
+tree.insert(7000);
+tree.insert(20000);
+prettyPrint(tree.root);
+console.log("------------------")
+tree.delete(67);
+prettyPrint(tree.root);
+console.log("------------------")
 
 
 
